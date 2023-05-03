da8b5ba7d6d162c2657821dee5ea3354ec0ebaab
Manuel Esteban Palacio Calderón <manuelpalacio@wss.com.co>
2023-05-03 14:41:14 -0500

    Run migration added address to restaurants

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_03_153935) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_03_193706) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,6 +30,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_03_153935) do
     t.integer "tables"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "address"
   end
 
   add_foreign_key "reservations", "restaurants"
