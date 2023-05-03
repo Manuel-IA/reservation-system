2dbb1286729a6a7855415241bac65598f499a0f4
Manuel Esteban Palacio Calderón <manuelpalacio@wss.com.co>
2023-05-03 15:21:20 -0500

    Run migration Added default value to tables restaurants

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_03_193706) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_03_195329) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,7 +27,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_03_193706) do
     t.string "description"
     t.string "city"
     t.string "photo_url"
-    t.integer "tables"
+    t.integer "tables", default: 15
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "address"
