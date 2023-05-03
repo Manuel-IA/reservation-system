cbbabcbb04497243b3ddcc34c042475df4782543
Manuel Esteban Palacio Calderón <manuelpalacio@wss.com.co>
2023-05-03 09:15:56 -0500

    Run migration for restaurants

--- db/schema.rb
+++ db/schema.rb
@@ -14,4 +14,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_02_223810) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "restaurants", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.string "city"
+    t.string "photo_url"
+    t.integer "tables"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
